08d43fc74b5a631152d025d125e87f92ab438dc9
Matthew Steinkamp <mdsteinkamp@gmail.com>
2022-12-14 08:17:28 -0800

    commit tree 91bf3efd0405af10fcfb5e93c02ff8d3b5e7ee6b

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let multiply;
+const num1 = 62;
+const num2 = 1;
+multiply = num1 * num2;
 
-// Write your code here
+let random;
+random = Math.floor(Math.random() * 10) +1;
+
+let mod;
+const num3 = 29;
+const num4 =5;
+mod = num3 % num4;
+
+const max = Math.max(1, 5, 10, 15, 20); 
